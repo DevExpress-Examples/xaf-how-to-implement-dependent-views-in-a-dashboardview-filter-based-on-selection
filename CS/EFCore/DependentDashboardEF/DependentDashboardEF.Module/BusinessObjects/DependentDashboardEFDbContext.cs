3f62578e8a360fe1c1586c0d95ddd127d2438ed7
andrey <andrey.kozhevnikov@devexpress.com>
2023-03-22 14:39:19 +0400

    EF

---
+++ CS/EFCore/DependentDashboardEF/DependentDashboardEF.Module/BusinessObjects/DependentDashboardEFDbContext.cs
@@ -0,0 +1,47 @@
+﻿using DevExpress.ExpressApp.EFCore.Updating;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using DevExpress.Persistent.BaseImpl.EF.PermissionPolicy;
+using DevExpress.Persistent.BaseImpl.EF;
+using DevExpress.ExpressApp.Design;
+using DevExpress.ExpressApp.EFCore.DesignTime;
+using DXApplication4.Module.BusinessObjects;
+
+namespace DependentDashboardEF.Module.BusinessObjects;
+
+// This code allows our Model Editor to get relevant EF Core metadata at design time.
+// For details, please refer to https://supportcenter.devexpress.com/ticket/details/t933891.
+public class DependentDashboardEFContextInitializer : DbContextTypesInfoInitializerBase {
+	protected override DbContext CreateDbContext() {
+		var optionsBuilder = new DbContextOptionsBuilder<DependentDashboardEFEFCoreDbContext>()
+            .UseSqlServer(";")
+            .UseChangeTrackingProxies()
+            .UseObjectSpaceLinkProxies();
+        return new DependentDashboardEFEFCoreDbContext(optionsBuilder.Options);
+	}
+}
+//This factory creates DbContext for design-time services. For example, it is required for database migration.
+public class DependentDashboardEFDesignTimeDbContextFactory : IDesignTimeDbContextFactory<DependentDashboardEFEFCoreDbContext> {
+	public DependentDashboardEFEFCoreDbContext CreateDbContext(string[] args) {
+		throw new InvalidOperationException("Make sure that the database connection string and connection provider are correct. After that, uncomment the code below and remove this exception.");
+		//var optionsBuilder = new DbContextOptionsBuilder<DependentDashboardEFEFCoreDbContext>();
+		//optionsBuilder.UseSqlServer("Integrated Security=SSPI;Pooling=false;Data Source=(localdb)\\mssqllocaldb;Initial Catalog=DependentDashboardEF");
+        //optionsBuilder.UseChangeTrackingProxies();
+        //optionsBuilder.UseObjectSpaceLinkProxies();
+		//return new DependentDashboardEFEFCoreDbContext(optionsBuilder.Options);
+	}
+}
+[TypesInfoInitializer(typeof(DependentDashboardEFContextInitializer))]
+public class DependentDashboardEFEFCoreDbContext : DbContext {
+	public DependentDashboardEFEFCoreDbContext(DbContextOptions<DependentDashboardEFEFCoreDbContext> options) : base(options) {
+	}
+    //public DbSet<ModuleInfo> ModulesInfo { get; set; }
+    public DbSet<MyTask> MyTasks { get; set; }
+    public DbSet<Contact> Contacts { get; set; }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder) {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.HasChangeTrackingStrategy(ChangeTrackingStrategy.ChangingAndChangedNotificationsWithOriginalValues);
+    }
+}
---
+++ CS/EFCore/DependentDashboardEF/DependentDashboardEF.Module/Controllers/DashboardFilterController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Linq;
+using DevExpress.ExpressApp;
+using DevExpress.Data.Filtering;
+using System.Collections.Generic;
+using DevExpress.ExpressApp.Editors;
+using DevExpress.ExpressApp.SystemModule;
+using DXApplication4.Module.BusinessObjects;
+
+namespace Solution3.Module.Controllers {
+    // For more typical usage scenarios, be sure to check out http://documentation.devexpress.com/#Xaf/clsDevExpressExpressAppViewControllertopic.
+    public class DashboardFilterController : ViewController<DashboardView> {
+        private const string DashboardViewId = "MyDashboardView";
+        private DashboardViewItem SourceItem;
+        private DashboardViewItem TargetItem;
+        private const string CriteriaName = "Test";
+
+        private void FilterDetailListView(ListView masterListView, ListView detailListView) {
+            detailListView.CollectionSource.Criteria.Clear();
+            List<object> searchedObjects = new List<object>();
+            foreach(object obj in masterListView.SelectedObjects) {
+                searchedObjects.Add(detailListView.ObjectSpace.GetKeyValue(obj));
+            }
+            if(searchedObjects.Count > 0) {
+                detailListView.CollectionSource.Criteria[CriteriaName] = CriteriaOperator.FromLambda<MyTask>(x => searchedObjects.Contains(x.AssignedTo.ID));
+            }
+        }
+        private void SourceItem_ControlCreated(object sender, EventArgs e) {
+            DashboardViewItem dashboardItem = (DashboardViewItem)sender;
+            ListView innerListView = dashboardItem.InnerView as ListView;
+            if(innerListView != null) {
+                innerListView.SelectionChanged -= innerListView_SelectionChanged;
+                innerListView.SelectionChanged += innerListView_SelectionChanged;
+            }
+        }
+        private void innerListView_SelectionChanged(object sender, EventArgs e) {
+            FilterDetailListView((ListView)SourceItem.InnerView, (ListView)TargetItem.InnerView);
+        }
+        private void DisableNavigationActions(Frame frame) {
+            RecordsNavigationController recordsNavigationController = frame.GetController<RecordsNavigationController>();
+            if (recordsNavigationController != null){
+                recordsNavigationController.Active.SetItemValue("DashboardFiltering", false);
+            }
+        }
+
+        protected override void OnActivated() {
+            base.OnActivated();
+            if(View.Id == DashboardViewId) {
+                SourceItem = (DashboardViewItem)View.FindItem(FilterSourceID);
+                TargetItem = (DashboardViewItem)View.FindItem(FilterTargetId);
+                if(SourceItem != null) {
+                    SourceItem.ControlCreated += SourceItem_ControlCreated;
+                }
+                if (TargetItem!=null)
+                if (TargetItem.Frame!=null)
+                    DisableNavigationActions(TargetItem.Frame);
+                else
+                    TargetItem.ControlCreated += (s, e) =>
+                    {
+                        DisableNavigationActions(TargetItem.Frame);
+                    };
+            }
+        }
+        protected override void OnDeactivated() {
+            if(SourceItem != null) {
+                SourceItem.ControlCreated -= SourceItem_ControlCreated;
+                SourceItem = null;
+            }
+            TargetItem = null;
+            base.OnDeactivated();
+        }
+        public DashboardFilterController() {
+            FilterSourceID = "ContactView";
+            FilterTargetId = "MyTaskView";
+        }
+        public string FilterSourceID { get; set; }
+        public string FilterTargetId { get; set; }
+    }
+}

06cbd5b9d13c348e69f4a40d21bb857343076642
AndreyKozhevnikov <andrey.kozhevnikov@devexpress.com>
2023-06-29 16:10:54 +0400

    Update Contact.cs

--- CS/XPO/DependentDashboard/DependentDashboard.Module/BusinessObjects/Contact.cs
+++ CS/XPO/DependentDashboard/DependentDashboard.Module/BusinessObjects/Contact.cs
@@ -53,16 +53,7 @@ namespace dxTestSolution.Module.BusinessObjects {
                 SetPropertyValue(nameof(Age), ref _age, value);
             }
         }
-        // DateTime _birthDate;
-        // public DateTime BirthDate {
-        // get {
-        // return _birthDate;
-        // }
-        // set {
-        // SetPropertyValue(nameof(BirthDate), ref _birthDate, value);
-        // }
-        // }	
-        //office#3		
+	
         [Association("Contact-Tasks")]
         public XPCollection<MyTask> Tasks {
             get {
@@ -70,21 +61,6 @@ namespace dxTestSolution.Module.BusinessObjects {
             }
         }
 		
-		        // byte[] _text;
-        // [EditorAlias(EditorAliases.RichTextPropertyEditor)]
-        // public byte[] Text {
-            // get {
-                // return _text;
-            // }
-            // set {
-                // SetPropertyValue(nameof(Text), ref _text, value);
-            // }
-        // }
-		// private byte[] data;
-        // [EditorAlias(EditorAliases.SpreadsheetPropertyEditor)] 
-        // public byte[] Data { 
-			// get { return data; }
-			// set { SetPropertyValue(nameof(Data), ref data, value); }
-		// }
+
     }
 }
